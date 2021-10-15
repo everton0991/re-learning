9f86594113d3ae7437e511e46569955bbdf5f729
Everton Renan <everton.renan.pc@gmail.com>
2021-10-15 09:40:33 -0300

    test: add snapshot tests for stateless components

---
+++ src/components/AddToList/AddToList.test.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import AddToList from './AddToList';
+
+it('renders correctly', () => {
+  const tree = render(
+    <AddToList
+      people={[{ name: '', age: 20, note: '', url: '' }]}
+      setPeople={jest.fn()}
+    />
+  );
+  expect(tree).toMatchSnapshot();
+});
